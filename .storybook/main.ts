bb6f1594e621a2ee6b5f8039a6edbab24023543d
David Zheng <hello@davidzheng.me>
2023-02-13 19:38:51 -0800

    fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,24 @@
 import { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y", "@storybook/addon-interactions", "@storybook/addon-docs", "@storybook/addon-coverage", "storybook-addon-pseudo-states"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@storybook/addon-interactions",
+    "@storybook/addon-docs",
+    "@storybook/addon-coverage",
+    "storybook-addon-pseudo-states",
+  ],
   framework: {
     name: "@storybook/react-vite",
-    options: {}
+    options: {},
   },
   features: {
-    interactionsDebugger: true
+    interactionsDebugger: true,
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
 export default config;
