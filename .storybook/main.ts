73a2b64ed5da5f97e090fa3eebbc72618f4b7c32
David Zheng <hello@davidzheng.me>
2023-02-13 19:26:53 -0800

    update pkgs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,16 @@
 import { StorybookConfig } from "@storybook/react-vite";
-
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
-    "@storybook/addon-interactions",
-    "@storybook/addon-docs",
-    "@storybook/addon-coverage",
-    "@storybook/addon-a11y",
-    "storybook-addon-pseudo-states",
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y", "@storybook/addon-interactions", "@storybook/addon-docs", "@storybook/addon-coverage", "storybook-addon-pseudo-states"],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {}
   },
   features: {
-    interactionsDebugger: true,
+    interactionsDebugger: true
   },
   docs: {
-    autodocs: true,
-  },
+    autodocs: true
+  }
 };
 export default config;
--- prettier.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  trailingComma: 'all',
-  tabWidth: 2,
-  semi: true,
-  printWidth: 120,
-  bracketSpacing: true,
-  tailwindConfig: 'tailwind.config.cjs',
-};
