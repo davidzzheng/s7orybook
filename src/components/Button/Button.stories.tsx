1e53b85b193abbf52cb994d8737a1a3be3b575e4
David Zheng <hello@davidzheng.me>
2023-02-13 19:56:56 -0800

    add button interaction

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { userEvent, within } from "@storybook/testing-library";
 
 import { Button } from "./Button";
 
@@ -45,6 +46,13 @@ export const Active: Story = {
   },
   parameters: {
     pseudo: { active: true },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = await within(canvasElement);
+
+    const btn = canvas.getByRole("button");
+
+    userEvent.click(btn);
   },
 };
 
